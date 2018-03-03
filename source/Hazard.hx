bf3bea19c3c335022c3d0ec72519315239e104e7
Mark Aldrich <mastermark51200@gmail.com>
2018-03-03 18:43:17 -0500

    Fixed typo

--- source/Hazard.hx
+++ source/Hazard.hx
@@ -11,5 +11,5 @@ interface Hazard {
 	// Is this hazard in a valid location? Will it only be
 	// affecting tiles that are in the map? This answer
 	// depends on the width and height of the segment
-	public function isValid(segment:MapSegment);
+	public function isValid(segment:MapSegment):Bool;
 }
